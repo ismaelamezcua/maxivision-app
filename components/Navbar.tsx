b8a60d0554bf10eb96d6e044d98a5fee32f35b80
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-07-07 13:33:22 -0500

    feat: add description titles on Navbar links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,11 +7,41 @@ const Navbar: FC = (): ReactElement => {
   const router = useRouter();
 
   const menuEntries = [
-    { key: 'subscribers', label: 'Suscriptores', href: '/', icon: <UsersIcon className="w-6 h-6" /> },
-    { key: 'contracts', label: 'Contratos', href: '/contratos', icon: <PencilAltIcon className="w-6 h-6" /> },
-    { key: 'transactions', label: 'Movimientos', href: '/movimientos', icon: <CreditCardIcon className="w-6 h-6" /> },
-    { key: 'reports', label: 'Reportes', href: '/reportes-servicio', icon: <ExclamationIcon className="w-6 h-6" /> },
-    { key: 'sales', label: 'Reporte de movimientos', href: '/reporte-movimientos', icon: <ClipboardListIcon className="w-6 h-6" /> },
+    {
+      key: 'subscribers',
+      label: 'Suscriptores',
+      href: '/',
+      description: 'Busca y agrega suscriptores',
+      icon: <UsersIcon className="w-6 h-6" />
+    },
+    {
+      key: 'contracts',
+      label: 'Contratos',
+      href: '/contratos',
+      description: 'Muestra detalles de contratos',
+      icon: <PencilAltIcon className="w-6 h-6" />
+    },
+    {
+      key: 'transactions',
+      label: 'Movimientos',
+      href: '/movimientos',
+      description: 'Realiza pagos',
+      icon: <CreditCardIcon className="w-6 h-6" />
+    },
+    {
+      key: 'reports',
+      label: 'Reportes',
+      href: '/reportes-servicio',
+      description: 'Muestra reportes de servicios',
+      icon: <ExclamationIcon className="w-6 h-6" />
+    },
+    {
+      key: 'sales',
+      label: 'Reporte de movimientos',
+      href: '/reporte-movimientos',
+      description: 'Muestra reportes de movimientos por dia',
+      icon: <ClipboardListIcon className="w-6 h-6" />
+    },
   ];
 
   return (
@@ -24,12 +54,12 @@ const Navbar: FC = (): ReactElement => {
             alt="Maxivision"
           />
           <div className="flex flewx-row space-x-4">
-            {menuEntries.map(({ key, label, href, icon }) => (
+            {menuEntries.map(({ key, label, href, description, icon }) => (
               <Link key={key} href={href}>
                 <a>
                   <div className={`text-blue-900 w-full p-2 rounded-t-lg inline-flex ${(router.asPath === href) ? 'bg-blue-200' : 'hover:bg-blue-200'} `}>
                     {icon}
-                    <span className="ml-1">{label}</span>
+                    <span className="ml-1" title={description}>{label}</span>
                   </div>
                 </a>
               </Link>
