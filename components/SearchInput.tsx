87f850ba906aea2a1d248e258c6d2d30fd640b60
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-07-17 15:26:53 -0500

    feat: refactor SearchInput component to allow placeholder prop

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -5,10 +5,19 @@ interface SearchInputProps {
   handleSubmit: (e: FormEvent) => void;
   handleReset: (e: FormEvent) => void;
   searchTerm: string;
+  placeholder: string;
   handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const SearchInput: FC<SearchInputProps> = ({ searchTerm, handleSubmit, handleReset, handleChange }): ReactElement => {
+const SearchInput: FC<SearchInputProps> = (props): ReactElement => {
+  const {
+    searchTerm,
+    placeholder,
+    handleSubmit,
+    handleReset,
+    handleChange,
+  } = props;
+
   return (
     <form
       className="w-full relative"
@@ -16,22 +25,22 @@ const SearchInput: FC<SearchInputProps> = ({ searchTerm, handleSubmit, handleRes
     >
       <span className="absolute inset-y-0 left-0 flex items-center pl-4 cursor-pointer">
         <SearchIcon
-          className={`${searchTerm ? 'text-gray-800' : 'text-gray-400'} w-5 h-5`}
+          className={`${searchTerm ? 'text-blue-600' : 'text-blue-700'} w-5 h-5`}
           onClick={handleSubmit}
         />
       </span>
 
-      <span className="absolute inset-y-0 right-0 flex items-center pr-4 cursor-pointer">
+      <span className="absolute inset-y-0 right-0 flex items-center pr-4 cursor-pointer ">
         {searchTerm
-          ? <XIcon className="w-5 h-5" onClick={handleReset} />
+          ? <XIcon className="w-5 h-5 hover:bg-gray-50" onClick={handleReset} />
           : <></>
         }
       </span>
 
       <input
         type="text"
-        className="w-full rounded-lg border-gray-400 focus:border-blue-900 hover:bg-gray-50 px-12 py-3"
-        placeholder="Busca por nombre o direcci&oacute;n"
+        className="w-full border-gray-400 focus:border-blue-700 px-12 py-3"
+        placeholder={placeholder}
         value={searchTerm}
         onChange={handleChange}
       />
