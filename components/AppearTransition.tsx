bb1600d81dc64041aaf41f2f7a5a139c7e698cc2
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-07-17 16:45:06 -0500

    feat: add AppearTransition component to animate appearing elements

---
+++ components/AppearTransition.tsx
@@ -0,0 +1,22 @@
+import { FC, ReactElement, ReactNode } from 'react';
+import { Transition } from '@headlessui/react';
+
+const AppearTransition: FC<{ children: ReactNode, show: boolean }> = (props): ReactElement => {
+  const { children, show } = props;
+
+  return (
+    <Transition
+      show={show}
+      enter="transition-opacity ease-linear duration-300"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity ease-linear duration-300"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      {children}
+    </Transition>
+  );
+};
+
+export default AppearTransition;
