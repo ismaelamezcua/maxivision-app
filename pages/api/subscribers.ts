280178a8ba8272aa32221bb270faf936428c3f8b
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-07-21 19:08:19 -0500

    feat: add GET, POST, and PUT method responses for /api/subscribers

--- pages/api/subscribers.ts
+++ pages/api/subscribers.ts
@@ -2,33 +2,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import type { Subscriber, ResponseError } from '../../types';
 import prisma from '../../lib/prisma';
 
-const responseData: Subscriber[] = [
-  { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-  { name: 'Courtney Henry', title: 'Designer', email: 'courtney.henry@example.com', role: 'Admin' },
-  { name: 'Tom Cook', title: 'Director, Product Development', email: 'tom.cook@example.com', role: 'Member' },
-  { name: 'Whitney Francis', title: 'Copywriter', email: 'whitney.francis@example.com', role: 'Admin' },
-  { name: 'Leonard Krasner', title: 'Senior Designer', email: 'leonard.krasner@example.com', role: 'Owner' },
-  { name: 'Floyd Miles', title: 'Principal Designer', email: 'floyd.miles@example.com', role: 'Member' },
-];
-
-// export default async function handle(req: NextApiRequest, res: NextApiResponse<Subscriber[] | ResponseError>) => {
-//   const { searchString } = req.query;
-
-//   const results = await prisma.subscriber.findMany({
-//     where: {
-//       name: {
-//         contains: searchString,
-//       },
-//     },
-//   });
-
-//   res.json(results);
-// }
-
 const subscriberApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
+  if (req.method === 'GET' || req.method === 'PUT' || req.method === 'DELETE') {
+    /* Method not supported for this API endpoint, use /api/search to retrieve subscribers */
+    res.status(501).json({ status: 'Method not supported by the server and cannot be handled.' });
+    return;
+  } else if (req.method === 'POST') {
     /* Create a new subscriber */
-    // const subscriberInfo = req.body;
     const { name, email, phone, rfc, spouse } = req.body;
 
     const subscriber = await prisma.subscriber.create({
@@ -41,10 +21,8 @@ const subscriberApiHandler = async (req: NextApiRequest, res: NextApiResponse) =
       },
     });
 
-    res.status(200).json(subscriber);
-  } else if (req.method === 'GET') {
-    console.log(req.query)
-    res.status(200).json(responseData);
+    res.status(201).json(subscriber);
+    return;
   }
 };
 
