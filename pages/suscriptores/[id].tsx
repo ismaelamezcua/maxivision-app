b07bd809ba0e4ca065beb8054e67cbbaae6f929c
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-07-23 20:39:37 -0500

    feat: create local component for rendering subscriber details

--- pages/suscriptores/[id].tsx
+++ pages/suscriptores/[id].tsx
@@ -1,14 +1,43 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { FC, ReactElement, useEffect, useState, VoidFunctionComponent } from 'react';
 import { Subscriber } from '@/types';
 import { ChevronLeftIcon, PencilAltIcon, SaveIcon } from '@heroicons/react/outline';
+
+interface DetailsInputProps {
+  label: string;
+  name: string;
+  value: string;
+  disabled: boolean;
+  onChange: () => void;
+};
+
+const DetailsInput: FC<DetailsInputProps> = (props): ReactElement => {
+  const { label, name, value, disabled, onChange } = props;
+
+  return (
+    <div className="flex space-x-4 w-full items-center mb-6">
+      <div className="basis-1/4">{label}</div>
+      <div className="flex-grow">
+        <input
+          className="form-input"
+          type={name === 'email'? 'email' : 'text'}
+          name={name}
+          value={value}
+          onChange={onChange}
+          disabled={!disabled}
+          required
+        />
+      </div>
+    </div>
+  );
+};
 
 const SubscriberDetails: NextPage = () => {
   const [subscriber, setSubscriber] = useState<Subscriber>();
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [nameDisabled, setNameDisabled] = useState<boolean>(true);
+  const [editMode, setEditMode] = useState<boolean>(false);
   const router = useRouter();
   const { id } = router.query;
 
@@ -47,32 +76,45 @@ const SubscriberDetails: NextPage = () => {
           </div>
 
           <div className="bg-white p-6">
+            {isLoading && (
+              <h1 className="font-bold">Loading data...</h1>
+            )}
             {subscriber && (
-              <div className="flex space-x-2 max-w-3xl mx-auto items-center">
-                <div className="basis-1/4">
-                  <p className="font-bold text-gray-600">Nombre</p>
-                </div>
-                <div className="flex-grow">
-                  <input
-                    className={`${!nameDisabled && 'bg-blue-100'} form-input`}
-                    type="text"
-                    name="name"
-                    value={subscriber.name}
-                    onChange={handleChange}
-                    disabled={nameDisabled}
-                    required
-                  />
-                </div>
-                <div
-                  className="px-2 py-3 hover:bg-gray-100 border border-white hover:border-gray-400 cursor-pointer"
-                  onClick={() => setNameDisabled(!nameDisabled)}
-                >
-                  {nameDisabled
-                    ? <PencilAltIcon className="w-6 h-6" />
-                    : <SaveIcon className="w-6 h-6" />
+              <>
+                <form>
+                  <DetailsInput label="Nombre" name="name" value={subscriber.name} disabled={editMode} onChange={handleChange} />
+                  <DetailsInput label="Correo electrónico" name="email" value={subscriber.email} disabled={editMode} onChange={handleChange} />
+                  <DetailsInput label="Teléfono" name="phone" value={subscriber.phone} disabled={editMode} onChange={handleChange} />
+                  <DetailsInput label="RFC" name="rfc" value={subscriber.rfc} disabled={editMode} onChange={handleChange} />
+                  <DetailsInput label="Cónyugue" name="spouse" value={subscriber.spouse} disabled={editMode} onChange={handleChange} />
+
+                  {!editMode
+                    ? (
+                      <div className="flex flex-row mt-6 justify-end">
+                        <div
+                          className="px-4 py-3 text-gray-800 border border-gray-400 hover:bg-gray-100 cursor-pointer inline-flex items-center"
+                          onClick={() => setEditMode(true)}
+                        >
+                          <PencilAltIcon className="w-5 h-5" />
+                          <span className="ml-2">Editar suscriptor</span>
+                        </div>
+                      </div>
+                    )
+                    : (
+                      <div className="flex flex-row mt-6 justify-end">
+                        <button
+                          type="submit"
+                          className="bg-blue-600 hover:bg-blue-700 px-4 py-3 text-white inline-flex items-center"
+                        >
+                          <SaveIcon className="w-5 h-5" />
+                          <span className="ml-2">Guardar cambios</span>
+                        </button>
+                      </div>
+                    )
                   }
-                </div>
-              </div>
+
+                </form>
+              </>
             )}
           </div>
         </div>
