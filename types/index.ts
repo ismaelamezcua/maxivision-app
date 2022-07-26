de368efd5933e462db7f6fad537d96a7924cf6f9
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-07-25 20:50:06 -0500

    refactor: define all interfaces in the same file

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,50 @@
-import type { Subscriber } from '@/types/subscriber';
-import type { Subscription } from '@/types/subscription';
+interface Subscriber {
+  id?: number;
+  createdAt?: string;
+  name: string;
+  email: string;
+  phone: string;
+  rfc: string;
+  spouse: string;
+};
 
-export {
+interface Subscription {
+  id?: number;
+  createdAt?: string;
+  updatedAt?: string;
+  address: string;
+  suburb: string;
+  identifier: string;
+  tvCount: number;
+  status: string;
+  cfe?: string;
+  remarks?: string;
+  subscriberId?: number;
+};
+
+interface Transaction {
+  id?: number;
+  createdAt?: string;
+  type: string;
+  price: number;
+  concept: string;
+  receiptId?: string;
+  subscriptionId?: number;
+};
+
+interface ServiceReport {
+  id?: number;
+  createdAt?: string;
+  updatedAt?: string;
+  description: string;
+  status: string;
+  comments: string;
+  subscriptionId?: number;
+};
+
+export type {
   Subscriber,
   Subscription,
+  Transaction,
+  ServiceReport,
 };
