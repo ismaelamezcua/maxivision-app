6c0d1e64d5bc5f6066b3c68a47130dd821cbf5a8
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-07-27 17:13:30 -0500

    feat: add transactions and results page

--- pages/transactions/index.tsx
+++ pages/transactions/index.tsx
@@ -1,121 +1,98 @@
+import { useState, ChangeEvent, FormEvent } from 'react';
+
 import { NextPage } from 'next';
 import Head from 'next/head';
 
-import {
-  useState,
-  useEffect,
-  ChangeEvent,
-  FormEvent,
-} from 'react';
+import { CalendarIcon, XIcon } from '@heroicons/react/outline';
 
-import SearchInput from '@/components/SearchInput';
-import ResultsTable from '@/components/subscribers/ResultsTable';
-import NewSubscriberModal from '@/components/subscribers/NewSubscriberModal';
-
-import { Subscriber } from '@/types';
-
-import { PlusIcon } from '@heroicons/react/outline';
+import { Transaction } from '@/types';
+import ResultsAndTotal from '@/components/transactions/ResultsAndTotal';
 
 const TransactionsPage: NextPage = () => {
-  const [term, setTerm] = useState<string>('');
+  const [date, setDate] = useState<string>('');
+  const [results, setResults] = useState<Transaction[]>([]);
   const [isFetching, setIsFetching] = useState<boolean>(false);
-  const [subscribers, setSubscribers] = useState<Subscriber[]>([]);
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-
-  // Check if localStorage has the searchResults object stored
-  useEffect(() => {
-    const searchResults = localStorage.getItem('searchResults');
-    if (searchResults !== null) {
-      setSubscribers(JSON.parse(searchResults));
-    }
-  }, []);
-
-  function closeModal() {
-    setIsModalOpen(false);
-  }
-
-  function openModal() {
-    setIsModalOpen(true);
-  }
 
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    setTerm(e.target.value);
-  }
+    const target = e.target.value;
+    setDate(target);
 
-  function handleSubmit(e: FormEvent) {
-    e.preventDefault();
-    if (!term) return;
-
-    setIsFetching(true);
-
-    fetch('/api/search', {
+    fetch('/api/transactions/search', {
       method: 'POST',
-      body: JSON.stringify({ term }),
+      body: JSON.stringify({ date: target }),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        'content-type': 'application/json',
+      },
     })
       .then(response => response.json())
-      .then(subscribers => {
-        setSubscribers(subscribers)
-        localStorage.setItem('searchResults', JSON.stringify(subscribers));
-        setIsFetching(false);
+      .then(results => {
+        setResults(results);
+        console.log(results);
       });
   }
 
   function handleReset(e: FormEvent) {
     e.preventDefault();
-    setTerm('');
+    setDate('');
   }
 
   const cleanTableResults = () => {
-    setSubscribers([]);
-    localStorage.removeItem('searchResults');
-    setTerm('');
+    setResults([]);
+    setDate('');
   }
 
   return (
     <>
       <Head>
-        <title>Maxivision - Suscriptores</title>
+        <title>Maxivision - Movimientos</title>
       </Head>
 
       <div className="container mx-auto max-w-6xl mt-6">
         <div className="flex flex-col space-y-6">
 
           <div className="flex flex-row justify-between items-center">
-            <h1 className="text-3xl font-bold text-slate-700">Suscriptores</h1>
-
-            <div
-              className="bg-blue-600 hover:bg-blue-700 px-4 py-3 text-white inline-flex items-center cursor-pointer"
-              onClick={openModal}
-            >
-              <PlusIcon className="w-5 h-5" />
-              <span className="ml-2">Agregar suscriptor</span>
-            </div>
+            <h1 className="text-3xl font-bold text-slate-700">Movimientos por d&iacute;a</h1>
           </div>
 
           <div className="bg-white p-6 shadow-md">
             <div className="mx-auto max-w-2xl">
-              <SearchInput
-                searchTerm={term}
-                placeholder="B&uacute;squeda por Nombre o Calle"
-                handleChange={handleChange}
-                handleSubmit={handleSubmit}
-                handleReset={handleReset}
-              />
+              <form
+                method="POST"
+                className="w-full relative"
+              >
+                <span className="absolute inset-y-0 left-0 flex items-center pl-4 cursor-pointer">
+                  <CalendarIcon
+                    className={`${date ? 'text-blue-600' : 'text-blue-700'} w-5 h-5`}
+                  />
+                </span>
+
+                <span className="absolute inset-y-0 right-0 flex items-center pr-4 cursor-pointer ">
+                  {date
+                    ? <XIcon className="w-5 h-5 hover:bg-gray-50" onClick={handleReset} />
+                    : <></>
+                  }
+                </span>
+
+                <input
+                  type="date"
+                  className="w-full border-gray-400 focus:border-blue-700 px-12 py-3"
+                  name="term"
+                  value={date}
+                  onChange={handleChange}
+                  required
+                />
+              </form>
             </div>
 
-            <ResultsTable
+            <ResultsAndTotal
               isFetching={isFetching}
-              subscribers={subscribers}
+              transactions={results}
               handleCleanResults={cleanTableResults}
             />
+
           </div>
         </div>
       </div>
-
-      <NewSubscriberModal isModalOpen={isModalOpen} closeModal={closeModal} />
     </>
   );
 
