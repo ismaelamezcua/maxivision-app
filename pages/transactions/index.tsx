61d37a48c760b0dcca6d338b047b2a5ae9baec39
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-07-27 00:46:50 -0500

    feat: add carbon-copy for new transaction page

---
+++ pages/transactions/index.tsx
@@ -0,0 +1,124 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+
+import {
+  useState,
+  useEffect,
+  ChangeEvent,
+  FormEvent,
+} from 'react';
+
+import SearchInput from '@/components/SearchInput';
+import ResultsTable from '@/components/subscribers/ResultsTable';
+import NewSubscriberModal from '@/components/subscribers/NewSubscriberModal';
+
+import { Subscriber } from '@/types';
+
+import { PlusIcon } from '@heroicons/react/outline';
+
+const TransactionsPage: NextPage = () => {
+  const [term, setTerm] = useState<string>('');
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [subscribers, setSubscribers] = useState<Subscriber[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  // Check if localStorage has the searchResults object stored
+  useEffect(() => {
+    const searchResults = localStorage.getItem('searchResults');
+    if (searchResults !== null) {
+      setSubscribers(JSON.parse(searchResults));
+    }
+  }, []);
+
+  function closeModal() {
+    setIsModalOpen(false);
+  }
+
+  function openModal() {
+    setIsModalOpen(true);
+  }
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setTerm(e.target.value);
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (!term) return;
+
+    setIsFetching(true);
+
+    fetch('/api/search', {
+      method: 'POST',
+      body: JSON.stringify({ term }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(subscribers => {
+        setSubscribers(subscribers)
+        localStorage.setItem('searchResults', JSON.stringify(subscribers));
+        setIsFetching(false);
+      });
+  }
+
+  function handleReset(e: FormEvent) {
+    e.preventDefault();
+    setTerm('');
+  }
+
+  const cleanTableResults = () => {
+    setSubscribers([]);
+    localStorage.removeItem('searchResults');
+    setTerm('');
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Maxivision - Suscriptores</title>
+      </Head>
+
+      <div className="container mx-auto max-w-6xl mt-6">
+        <div className="flex flex-col space-y-6">
+
+          <div className="flex flex-row justify-between items-center">
+            <h1 className="text-3xl font-bold text-slate-700">Suscriptores</h1>
+
+            <div
+              className="bg-blue-600 hover:bg-blue-700 px-4 py-3 text-white inline-flex items-center cursor-pointer"
+              onClick={openModal}
+            >
+              <PlusIcon className="w-5 h-5" />
+              <span className="ml-2">Agregar suscriptor</span>
+            </div>
+          </div>
+
+          <div className="bg-white p-6 shadow-md">
+            <div className="mx-auto max-w-2xl">
+              <SearchInput
+                searchTerm={term}
+                placeholder="B&uacute;squeda por Nombre o Calle"
+                handleChange={handleChange}
+                handleSubmit={handleSubmit}
+                handleReset={handleReset}
+              />
+            </div>
+
+            <ResultsTable
+              isFetching={isFetching}
+              subscribers={subscribers}
+              handleCleanResults={cleanTableResults}
+            />
+          </div>
+        </div>
+      </div>
+
+      <NewSubscriberModal isModalOpen={isModalOpen} closeModal={closeModal} />
+    </>
+  );
+
+};
+
+export default TransactionsPage;
